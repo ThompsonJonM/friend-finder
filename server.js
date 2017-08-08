2b4cdb56fcd2a35e3fdb1785affab83e5954078c
unknown <thompson.jonm@gmail.com>
2017-08-08 15:42:01 -0500

    server up and running

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+// Dependencies
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
 var PORT = 3000;
 
 var app = express();
 
+// Body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Home Route
 app.get('/', function(req, res) {
-    res.send('Friend Finder Op');
+    res.sendFile(path.join(__dirname + "/public/home.html"));
+});
+
+// Survey Route
+app.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/survey.html"));
 });
 
 app.listen(PORT, function() {
